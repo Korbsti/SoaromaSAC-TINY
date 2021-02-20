781f918f04d00851e953637d216e985319c19b28
Korbsti <73249443+Korbsti@users.noreply.github.com>
2021-02-19 22:18:59 -0500

    Create reach.java

---
+++ reach.java
@@ -0,0 +1,17 @@
+			//EntityDamageByEntityEvent event
+      Entity taking = event.getEntity();
+      int limit = 3.5; 
+			Location locationDamager = player.getLocation();
+			Location locationDamagee = taking.getLocation();
+			double LocationDamagerX = locationDamager.getX();
+			double LocationDamagerY = locationDamager.getY() + 1.0;
+			double LocationDamagerZ = locationDamager.getZ();
+			double LocationDamageeX = locationDamagee.getX();
+			double LocationDamageeY = locationDamagee.getY();
+			double LocationDamageeZ = locationDamagee.getZ();
+			double range;
+			range = Math.sqrt(
+					Math.pow(LocationDamagerX - LocationDamageeX, 2) + Math.pow(LocationDamagerY - LocationDamageeY, 2)+ Math.pow(LocationDamagerZ - LocationDamageeZ, 2));
+			if (range >= limit) {
+        //flag player
+			}
